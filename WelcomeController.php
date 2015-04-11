5762048f92709d7c06c309b828ea985442c02ed5
Kruizi <bred1996@ya.ru>
2015-04-11 22:36:49 +0300

    Исправленный контроллер

--- WelcomeController.php
+++ WelcomeController.php
@@ -8,30 +8,66 @@ class WelcomeController extends Controller {
      */
     public function index()
     {
+        /** Ссылка на сайт(Она может быть любая, потом просто переправить метод откуда ее брать) */
         $url = 'http://php.net/';
+        /** Отправляем ссылку в метод который вернет страницу */
         $isDomainAvailible = $this->isDomainAvailible($url);
+        /** Проверяем на пустоту */
         if (!empty($isDomainAvailible)) {
+            /** Делаем цикл, где указываем сколько элементов в массиве и указываем ему [1] то есть на готовые ссылки
+             * бзе html
+             */
             for($i = 0; $i < count($isDomainAvailible['1']); $i++)
             {
+                /** Если попадутся ссылки подобия "http://www.php.net" или "https://"*/
                 if(strripos($isDomainAvailible['1'][$i], 'http') == 'true')
                 {
+                    /** Добавляем в бд уже обрезаную строку с помощью функции substr and strlen */
                     People::insertGetId(
                         array('desc' => ''.substr($isDomainAvailible['1'][$i], strlen($url)).'')
                     );
                 }else
                 {
+                    /** Ну если нету то добавляем просто ссылку */
                     People::insertGetId(
                         array('desc' => ''.$isDomainAvailible['1'][$i].'')
                     );
                 }
                 /**/
             }
+            /** Выводит ссылки */
             $good = People::get();
         }else
         {
+            /** Если пришел пустой массив выведем ошибкуу */
             $good = 'Error array';
         }
         return view('index', compact('good'));
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     * Проверяем ссылки на доступность
+     */
+    public function checkUrl()
+    {
+        /** Берем количество ссылок в базе */
+        $countUrl = People::where('admin', '=', 0)->count();
+        /** Сделаем цикл для обновлении ссылок */
+        for($i = 1; $i < $countUrl; $i++)
+        {
+            /** Берем саму ссылку */
+            $users = People::where('id', '=', $i)->addSelect('desc')->get();
+            foreach($users as $u)
+            {
+                /** Проверяем на статус */
+                $goods = get_headers('http://php.net/'.$u->desc);
+                /** Обновляем */
+                People::where('id', '=', $i)->update(['name' => $goods[0]]);
+            }
+
+        }
+        return view('check', compact('goods'));
     }
 
     /**
@@ -41,7 +77,7 @@ class WelcomeController extends Controller {
     private function isDomainAvailible($url)
     {
         $ch = curl_init();
-        curl_setopt ($ch, CURLOPT_URL,$url);
+        curl_setopt ($ch, CURLOPT_URL,$url);// ссылка
         curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"); //агент которым мы представимся
         curl_setopt ($ch, CURLOPT_TIMEOUT, 15 ); // таймаут
         curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 0);
