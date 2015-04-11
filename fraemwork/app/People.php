5a831bb268c704f51e1caff81b5052c4e62fa2df
Kruizi <bred1996@ya.ru>
2015-04-11 09:57:30 +0400

    Ларочка

---
+++ fraemwork/app/People.php
@@ -0,0 +1,6 @@
+<?php namespace Laravel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class People extends Model {
+}
---
+++ fraemwork/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+    <h1>{{$good}}</h1>
+</div>
+@endsection
